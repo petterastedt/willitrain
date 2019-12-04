c95a5466a4e235e060ff4d0fca48bb2dde7a9acd
petterastedt <petter.astedt@gmail.com>
2019-12-04 19:56:11 +0100

    Updates

---
+++ precache-manifest.d8e174d026094b72b3c2ffc1814d5a82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfccb42c675ecdc8473b1f3c4cce1b59",
+    "url": "/willitrain/index.html"
+  },
+  {
+    "revision": "37b013c2bb987c5b78fd",
+    "url": "/willitrain/static/css/main.3c94b3b2.chunk.css"
+  },
+  {
+    "revision": "679cd38a5122d8cee783",
+    "url": "/willitrain/static/js/2.d815c448.chunk.js"
+  },
+  {
+    "revision": "37b013c2bb987c5b78fd",
+    "url": "/willitrain/static/js/main.a15ff859.chunk.js"
+  },
+  {
+    "revision": "8a497bff22cbc2bc3ee9",
+    "url": "/willitrain/static/js/runtime-main.e3bfa4b8.js"
+  },
+  {
+    "revision": "8b92f956b97b9368885efb00480adece",
+    "url": "/willitrain/static/media/rainy.8b92f956.svg"
+  }
+]);
